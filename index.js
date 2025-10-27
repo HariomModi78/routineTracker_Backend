d955f075a8411136545034cf9edaff421e3a6796
Hariom Modi <hariommodi7898@gmail.com>
2025-10-27 09:30:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
